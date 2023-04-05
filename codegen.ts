881fb607b9f236566d7d7e7e72d932274f5ea30c
Chin <chin@Trinhs-MacBook-Pro.local>
2023-04-05 10:21:43 +0700

    Bug: fix codegen server

--- codegen.ts
+++ codegen.ts
@@ -1,17 +1,18 @@
-
-import type { CodegenConfig } from '@graphql-codegen/cli';
+import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   overwrite: true,
   // schema: process.env.VITE_ROOT_URL,
-  schema: process.env.VITE_BASE_URL,
+  schema:
+    process.env.VITE_BASE_URL ??
+    "https://graphql.staging.jsb.groovetechnology.co/api/graphql",
   documents: "src/**/*.tsx",
   generates: {
     "src/gql/": {
       preset: "client",
-      plugins: []
+      plugins: [],
     },
-  }
+  },
 };
 
 export default config;

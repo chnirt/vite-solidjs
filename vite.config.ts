9c8ff19a45ce26fa0b9154a23097add287fcf0cd
Chin <chin@Trinhs-MacBook-Pro.local>
2023-04-07 11:41:54 +0700

    Feature: update code

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,9 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3001,
+    port: 3000,
   },
   preview: {
-    port: 3000,
+    port: 3001,
   },
 });

0854a8bfdd1709def7caee19d522c86c0b966340
Chin <chin@Trinhs-MacBook-Pro.local>
2023-04-13 13:21:33 +0700

    Feature: handle dashboard ui

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -7,6 +7,8 @@ import { paths } from "../routes/constant";
 import clsx from "clsx";
 import CNTransition from "../components/CNTransition";
 
+const appName = "Groove Technology" ?? "Windmill";
+
 const navLinks = [
   {
     text: "Todos",
@@ -67,8 +69,7 @@ const Home = () => {
             className="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200"
             href="#"
           >
-            {/* Windmill */}
-            Groove Technology
+            {appName}
           </a>
           <ul className="mt-6">
             <li className="relative px-6 py-3">
@@ -377,7 +378,7 @@ const Home = () => {
             className="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200"
             href="#"
           >
-            Windmill
+            {appName}
           </a>
           <ul className="mt-6">
             <li className="relative px-6 py-3">
@@ -385,9 +386,10 @@ const Home = () => {
                 className="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
                 aria-hidden="true"
               />
-              <a
+              <NavLink
                 className="inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100"
-                href="index.html"
+                // href="index.html"
+                to={paths.dashboard}
               >
                 <svg
                   className="w-5 h-5"
@@ -402,11 +404,34 @@ const Home = () => {
                   <path d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
                 </svg>
                 <span className="ml-4">Dashboard</span>
-              </a>
+              </NavLink>
             </li>
           </ul>
           <ul>
-            <li className="relative px-6 py-3">
+            {navLinks.map((navLink, nli) => (
+              <li key={`mobile-nav-link-${nli}`} className="relative px-6 py-3">
+                <NavLink
+                  className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                  // href="forms.html"
+                  to={navLink.to}
+                >
+                  <svg
+                    className="w-5 h-5"
+                    aria-hidden="true"
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
+                  </svg>
+                  <span className="ml-4">{navLink.text}</span>
+                </NavLink>
+              </li>
+            ))}
+            {/* <li className="relative px-6 py-3">
               <a
                 className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
                 href="forms.html"
@@ -600,7 +625,7 @@ const Home = () => {
                   </li>
                 </ul>
               )}
-            </li>
+            </li> */}
           </ul>
           <div className="px-6 my-6">
             <button className="flex items-center justify-between px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
